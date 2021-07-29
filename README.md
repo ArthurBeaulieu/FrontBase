455f34ddb474f38de2ccedd6ab6370c465d16ecd
ArthurBeaulieu <contact@abeaulieu.fr>
2021-07-29 14:41:23 +0200

    windows batch almost ok

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # FrontBase
-An up and running template with doc, tests and bundling already set up!
+
+This template was built to serve as a template for a JavaScript es6 component. It offers a a js and scss bundling process (webpack), a test instrastructure (based on Jasmine and Karma), a documentation generator all set up (jsDoc) and a html demo page that loads the newly created component.
+
+It works on both Windows and Unix based system ; on Windows, double click the `init.bat` file and on Unix systems, run the `init.sh` script to set up the component environement.
--- init.bat
+++ init.bat
@@ -1,6 +1,6 @@
 @set vers=0.1.0
 
-:: Scrip welcome message and explanation
+:: Script welcome message and explanation
 
 echo OFF
 echo ## ---------------------------------- ##
@@ -16,15 +16,21 @@ echo.
 
 :: Ask user mandatory information
 
-set /p username="What is your username (GitHub account or else) ? "
+set /p username="What is your GitHub username ? "
 set /p component="What is the name of this project ? "
+set /p description="How would you describe it ? "
 set /p version="What is its version number ? "
+set /p license="Which distribution license do you want to use ? "
 
-:: Creating .scss and .js files
+:: Creating .scss and .js files (source and test)
 
 echo.
-echo Creating js and scss files in src/js and src/scss subdirectory
-mkdir src && mkdir src\js && mkdir src\scss
+echo Handling js and scss files in src/js, test file and src/scss subdirectories
+echo  -^> Creating required subdirectories
+if not exist src ( mkdir src )
+if not exist src\js ( mkdir src\js )
+if not exist src\scss ( mkdir src\scss )
+echo  -^> Creating source files
 echo OFF
 echo * { box-sizing: border-box }>%CD%\src\scss\%component%.scss
 (
@@ -36,33 +42,48 @@ echo * { box-sizing: border-box }>%CD%\src\scss\%component%.scss
 	echo.
 	echo export default %component%;
 )>%CD%\src\js\%component%.js
+(
+	echo import '../src/js/%component%.js';
+	echo.
+	echo describe^('%component% test', ^(^) =^> {
+	echo   it^('Unit test', done =^> {
+	echo     done^(^);
+	echo   }^);
+	echo }^);
+)>%CD%\test\%component%.spec.js
 echo Source files successfully created
 
 :: Replacing strings in files to properly prepare the folder
 
 echo.
 echo Fill configuration files with the information you provided
-echo Replacing in demo/example.html
+echo  -^> Replacing in demo/example.html
 call :replaceInFile demo\example.html COMPONENT %component%
 call :replaceInFile demo\example.html VERSION %version%
-
-echo Replacing in doc/jsDoc.json
+echo  -^> Replacing in doc/jsDoc.json
 call :replaceInFile doc/jsDoc.json COMPONENT %component%
-
-echo Replacing in webpack/plugins.js
+echo  -^> Replacing in webpack/plugins.js
 call :replaceInFile webpack/plugins.js COMPONENT %component%
-
-echo Replacing in webpack/webpack.common.js
+echo  -^> Replacing in webpack/webpack.common.js
 call :replaceInFile webpack/webpack.common.js COMPONENT %component%
-
-echo Replacing in package.json
+echo  -^> Replacing in package.json
 call :replaceInFile package.json USERNAME %username%
 call :replaceInFile package.json COMPONENT %component%
-call :replaceInFile package.json VERSION %version%
+call :replaceInFile package.json DESCRIPTION %description%
+call :replaceInFile package.json PROJECT_VERSION %version%
+call :replaceInFile package.json PROJECT_LICENSE %license%
 echo Configuration files are up and ready
+
+:: Clear README.md and prepare it with user information
+
+echo.
+echo Editing README.md to match project information
+call :clearMD
+echo README.md file now reflect the new project
 
 :: Using npm install if any, display error otherwise
 
+echo.
 set installed=
 call :isNpmInstalled installed
 if %installed% == no (
@@ -70,18 +91,17 @@ if %installed% == no (
 	exit /B -1
 )
 
-echo.
-echo Running npm install to finalize component installation
-npm install
+echo Running npm install to install component dependencies
+call npm install
+call npm run build
 
 :: Clearing both .bat and .sh files
+
 echo.
-echo This script will now self-destruct to let you a properly set up dev environment
+echo This script will now self-destruct to let you a properly use this dev environment
+echo You can now start to develop. See package.json scripts commands for usage
 del init.sh
 (goto) 2>nul & del "%~f0"
-
-echo.
-
 exit /B %ERRORLEVEL%
 
 :: Function definition
@@ -92,6 +112,23 @@ set INTEXTFILE=%~1
 set SEARCHTEXT=%~2
 set REPLACETEXT=%~3
 powershell -NoP -C "(Get-Content $ENV:INTEXTFILE) -Replace $ENV:SEARCHTEXT,$ENV:REPLACETEXT|Set-Content $ENV:INTEXTFILE"
+goto :eof
+
+:clearMD
+break>README.md
+echo OFF
+(
+	echo ^# %component%
+	echo.
+	echo ![]^(https://badgen.net/badge/version/%license%/blue^)
+	echo ![License]^(https://img.shields.io/github/license/%username%/%component%.svg^)
+	echo ![Doc]^(https://badgen.net/badge/documentation/TODO/orange^)
+	echo ![Test]^(https://badgen.net/badge/test/TODO/orange^)
+	echo.
+	echo %description%
+	echo.
+	echo %component% %version% - %license% - %username%
+)>%CD%\README.md
 goto :eof
 
 :isNpmInstalled
